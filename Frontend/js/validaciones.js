0e774bb96c82c5f476acfb841850cb74f84ff996
Aby :) <sanchezdeniseabril@gmail.com>
2023-08-23 13:45:00 -0300

    probando

--- Frontend/js/validaciones.js
+++ Frontend/js/validaciones.js
@@ -1,78 +1,68 @@
 const URL_BASE = "http://localhost:3000/";
 
 
-function IniciarSesion(email , password)
-{
+function IniciarSesion(email, password) {
     $.ajax(
-    {
-        "url": URL_BASE + "usuario",
-        "type": "POST",
-        "dataType": "json",
-        "data": 
         {
-            "email": email,
-            "password": password,   
-        },
-        success: function(r)
-        {
-            if(r.error == 0)
+            "url": URL_BASE + "usuario",
+            "type": "POST",
+            "dataType": "json",
+            "data":
             {
-                sessionStorage.setItem("token_usuario" , r.token);
-                //location.href="";
-            } else{
-                alert("INCORRECTO :(");
-            }
-       
-        },
-    });
+                "email": email,
+                "password": password,
+            },
+            success: function (r) {
+                if (r.error == 0) {
+                    sessionStorage.setItem("token_usuario", r.token);
+                    //location.href="";
+                } else {
+                    alert("INCORRECTO :(");
+                }
+
+            },
+        });
 };
 
-function Validacion()
-{
+function Validacion() {
     var token_usuario = sessionStorage.getItem("token_usuario");
 
     $.ajax(
-    {
-        "url": URL_BASE + "usuariovalidado",
-        "type": "POST",
-        "dataType": "json",
-        "data": 
         {
-            "token": token_usuario,   
-        },
-        success: function(r)
-        {
-            if(r.error == 0)
+            "url": URL_BASE + "usuariovalidado",
+            "type": "POST",
+            "dataType": "json",
+            "data":
             {
-                //todo bien validado
-            } else{
-                //location.href="PATEADO.html" 
-            }
-       
-        },
-    });
+                "token": token_usuario,
+            },
+            success: function (r) {
+                if (r.error == 0) {
+                    //todo bien validado
+                } else {
+                    //location.href="PATEADO.html" 
+                }
+
+            },
+        });
 
 };
 
-function RegistrarUsuario(email , password)
-{
+function RegistrarUsuario(email, password) {
     $.ajax(
         {
             "url": URL_BASE + "registrousuario",
             "type": "POST",
             "dataType": "json",
-            "data": 
+            "data":
             {
-                "email": email,   
+                "email": email,
                 "password": password,
             },
-            success: function(r)
-            {
-                if(r.error == 1)
-                {
+            success: function (r) {
+                if (r.error == 1) {
                     alert(r.mensaje);
-                } else
-                {
+                } else {
                     alert("Su usuario se ha creado exitosamente");
                 }
             },
