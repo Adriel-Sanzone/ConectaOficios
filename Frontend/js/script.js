23ef6e13e48aa7c3eb0b7904e7235952054dc669
Aby :) <sanchezdeniseabril@gmail.com>
2023-08-25 20:26:06 -0300

    diseño responsive que no anda xd

--- Frontend/js/script.js
+++ Frontend/js/script.js
@@ -1,3 +1,4 @@
+//Boton en registro
 document.addEventListener("DOMContentLoaded", function () {
     const mostrarCamposCheckbox = document.getElementById("mostrarCampos");
     const camposAdicionalesDiv = document.getElementById("camposAdicionales");
@@ -9,4 +10,20 @@ document.addEventListener("DOMContentLoaded", function () {
             camposAdicionalesDiv.style.display = "none";
         }
     });
+});
+
+//Mostrar u ocultar el menú al hacer clic en el icono de hamburguesa
+
+const mobileMenuButton = document.getElementById("mobile-menu-button");
+const menuDiv = document.getElementById("menu");
+
+mobileMenuButton.addEventListener("click", function() {
+    menuDiv.classList.toggle("show-menu");
+});
+
+// Agregar un listener para cambiar las clases en función del ancho de la ventana
+window.addEventListener("resize", function() {
+    if (window.innerWidth >= 768) {
+        menuDiv.classList.remove("show-menu");
+    }
 });
