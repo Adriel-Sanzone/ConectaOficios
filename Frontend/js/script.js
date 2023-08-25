0ebdc05d519296b5a34d17384e2ee02ef652dc98
Aby :) <sanchezdeniseabril@gmail.com>
2023-08-24 22:55:19 -0300

    creacion de login

---
+++ Frontend/js/script.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const mostrarCamposCheckbox = document.getElementById("mostrarCampos");
+    const camposAdicionalesDiv = document.getElementById("camposAdicionales");
+
+    mostrarCamposCheckbox.addEventListener("change", function () {
+        if (mostrarCamposCheckbox.checked) {
+            camposAdicionalesDiv.style.display = "block";
+        } else {
+            camposAdicionalesDiv.style.display = "none";
+        }
+    });
+});
