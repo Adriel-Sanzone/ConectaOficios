9d0f52de1bb3d0179fdaf91842a545aa2434142e
Adriel <adrielar2000@gmail.com>
2023-09-05 18:01:42 -0300

    Arreglos para exponer

--- Backend/Routes/views.js
+++ Backend/Routes/views.js
@@ -4,5 +4,14 @@ const router = Router();
 router.get('/', function (req, res) {
     res.render('../Frontend/views/pages/index')
 });
+router.get('/iniciarSesion', function (req, res) {
+    res.render('../Frontend/views/pages/iniciarSesion')
+});
+router.get('/oficios', function (req, res) {
+    res.render('../Frontend/views/pages/oficios')
+});
+router.get('/registro', function (req, res) {
+    res.render('../Frontend/views/pages/registro')
+});
 
 export default router;
