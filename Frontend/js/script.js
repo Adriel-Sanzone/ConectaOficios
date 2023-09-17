7b60ff8fecd2b6e5189c2d6e4f0c922ada767fd6
Aby :) <sanchezdeniseabril@gmail.com>
2023-09-17 16:18:11 -0300

    arreglos en perfil

--- Frontend/js/script.js
+++ Frontend/js/script.js
@@ -12,32 +12,19 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 });
 
-//aspecto de foto en carrusel para que se mantenga cuadrada
-const aspectRatio1to1 = document.querySelectorAll('.aspect-ratio-1-1');
+//oculta containers en perfil
+const iconos = document.querySelectorAll('.icono-perfil'); // Obtén todos los elementos con la clase .icono
+const contenedoresInfo = document.querySelectorAll('.contenedor-info'); // Obtén todos los elementos con la clase .contenedor-info
 
-// Funcion para el evento
-function resizeForSquareAppearance(event) {
-    aspectRatio1to1.forEach((element) => {
-        // Modifica la altura a partir de la anchura del elemento
-        element.style.height = `${element.clientWidth}px`;
-    })
-}
-window.addEventListener('resize', resizeForSquareAppearance);
-document.addEventListener('DOMContentLoaded', resizeForSquareAppearance);
-
-
-
-
+// Agrega un evento de clic a cada icono
+iconos.forEach((icono, index) => {
+    icono.addEventListener('click', () => {
+        if (contenedoresInfo[index].style.display === 'none' || contenedoresInfo[index].style.display === '') {
+            contenedoresInfo[index].style.display = 'block';
+        } else {
+            contenedoresInfo[index].style.display = 'none';
+        }
+    });
+});
 
 
-
-//Declarando variables
-bars_search =       document.getElementById("ctn-bars-search");
-cover_ctn_search =  document.getElementById("cover-ctn-search");
-inputSearch =       document.getElementById("inputSearch");
-box_search =        document.getElementById("box-search");
-
-function buscador_interno(){
-    filter = inputSearch.value.toUpperCase();
-    
-}
