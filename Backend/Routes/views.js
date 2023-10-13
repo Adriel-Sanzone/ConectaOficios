0caf97825293eabc8420645cc7b540d04d7ab19f
Aby :) <sanchezdeniseabril@gmail.com>
2023-10-13 13:57:11 -0300

    intento de hacer funcionales tablas admin

--- Backend/Routes/views.js
+++ Backend/Routes/views.js
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import {viewUsuariosEspecialistas, UsuarioLogeado, getUsuario, viewsTodosLosUsuarios, viewProyectos} from '../Controllers/usuarios.js'
+import { viewUsuariosEspecialistas, UsuarioLogeado, getUsuario, viewsTodosLosUsuarios, viewProyectos } from '../Controllers/usuarios.js'
 import { viewEspecializacionUsuario, viewEspecializaciones, getEspecializacionPerfil, viewEspecializacionesNueva } from '../Controllers/especializaciones.js';
 
 const router = Router();
@@ -9,30 +9,26 @@ router.get('/', function (req, res) {
     var idUsuario = (req.session.idUsuario || 0);
     var tokenUsuario = (req.session.token || 0);
 
-    var usuarioLogeado = UsuarioLogeado(idUsuario , tokenUsuario);
+    var usuarioLogeado = UsuarioLogeado(idUsuario, tokenUsuario);
 
     var usuariosEspecialistas = viewUsuariosEspecialistas(0);
     var especializaciones = viewEspecializaciones();
     var especializacionUsuario = viewEspecializacionUsuario();
-    usuariosEspecialistas.then(function(usuarios)
-    {
-        especializaciones.then(function(especializacion)
-        {
-            especializacionUsuario.then(function(userEspecializacion)
-            {
-                usuarioLogeado.then(function(userLogeado)
-                {
+    usuariosEspecialistas.then(function (usuarios) {
+        especializaciones.then(function (especializacion) {
+            especializacionUsuario.then(function (userEspecializacion) {
+                usuarioLogeado.then(function (userLogeado) {
                     res.render("../Frontend/views/pages/index", {
-                        "usuariosEsp" : usuarios,
-                        "especializacion" : especializacion,
-                        "especializacionUsuario" : userEspecializacion,
+                        "usuariosEsp": usuarios,
+                        "especializacion": especializacion,
+                        "especializacionUsuario": userEspecializacion,
                         "usuarioLogeado": userLogeado,
                     });
                 })
             })
         })
-    }) 
-}); 
+    })
+});
 
 
 router.get('/iniciarSesion', function (req, res) {
@@ -46,38 +42,34 @@ router.get('/oficios/:pag', function (req, res) {
     var idUsuario = (req.session.idUsuario || 0);
     var tokenUsuario = (req.session.token || 0);
 
-    var usuarioLogeado = UsuarioLogeado(idUsuario , tokenUsuario);
+    var usuarioLogeado = UsuarioLogeado(idUsuario, tokenUsuario);
 
     //var usuariosEspecialistas = viewUsuariosEspecialistas(pagina);
     var usuariosEspecialistas = viewsTodosLosUsuarios(pagina, categoria);
 
     //var especializaciones = viewEspecializaciones();
     var especializaciones = viewEspecializacionesNueva();
-    usuariosEspecialistas.then(function(usuarios)
-    {
-        especializaciones.then(function(especializacion)
-        {
+    usuariosEspecialistas.then(function (usuarios) {
+        especializaciones.then(function (especializacion) {
             console.log(especializacion);
-            usuarioLogeado.then(function(logeado)
-            {
+            usuarioLogeado.then(function (logeado) {
                 res.render("../Frontend/views/pages/oficios", {
-                    "usuariosEsp" : usuarios,
-                    "especializacion" : especializacion,
+                    "usuariosEsp": usuarios,
+                    "especializacion": especializacion,
                     "usuarioLogeado": logeado,
                 });
             })
         })
-    }) 
+    })
 });
 
 router.get('/registro', function (req, res) {
     var especializaciones = viewEspecializaciones()
-    especializaciones.then(function(especializacion)
-    {
+    especializaciones.then(function (especializacion) {
         res.render("../Frontend/views/pages/registro", {
-            "especializacion" : especializacion,
+            "especializacion": especializacion,
         });
-    }) 
+    })
 });
 
 router.get('/registro2', function (req, res) {
@@ -89,7 +81,7 @@ router.get('/perfil/:id', function (req, res) {
     var idUsuario = (req.session.idUsuario || 0);
     var tokenUsuario = (req.session.token || 0);
 
-    var usuarioLogeado = UsuarioLogeado(idUsuario , tokenUsuario);
+    var usuarioLogeado = UsuarioLogeado(idUsuario, tokenUsuario);
 
     var idPerfil = req.params.id;
     var idUsuario = (req.session.idUsuario || 0);
@@ -97,14 +89,10 @@ router.get('/perfil/:id', function (req, res) {
     var especializacionPerfil = getEspecializacionPerfil(idPerfil);
     var proyectosPerfil = viewProyectos(idPerfil);
 
-    usuarioPerfil.then(function(usuario)
-    {
-        especializacionPerfil.then(function(especializaciones)
-        {
-            usuarioLogeado.then(function(logeado)
-            {
-                proyectosPerfil.then(function(proyectos)
-                {
+    usuarioPerfil.then(function (usuario) {
+        especializacionPerfil.then(function (especializaciones) {
+            usuarioLogeado.then(function (logeado) {
+                proyectosPerfil.then(function (proyectos) {
                     res.render('../Frontend/views/pages/perfil', {
                         "user": usuario,
                         "id": idUsuario,
@@ -113,10 +101,10 @@ router.get('/perfil/:id', function (req, res) {
                         "proyectos": proyectos,
                     });
                 })
-            })      
+            })
         })
-  
-    })   
+
+    })
 });
 
 router.get('/admin', function (req, res) {
@@ -132,6 +120,20 @@ router.get('/destacar', function (req, res) {
 router.get('/categorias', function (req, res) {
     res.render('../Frontend/views/pages/tablas-admin/categorias')
 });
+
+// router.get('/categorias', (req, res) => {
+//     const queryCategorias = 'SELECT * FROM categorias';
+//     const resultados = {};
+//     connection.query(queryCategorias, (errorCategorias, resultadosCategorias) => {
+//         if (errorCategorias) {
+//             console.error('Error al obtener los datos de la tabla "Categorias":', errorCategorias.stack);
+//             res.sendStatus(500); // Envía una respuesta de error al cliente
+//             return;
+//         }
+//         resultados.categorias = resultadosCategorias;
+//         res.render('categorias', { datos: resultados });
+//     })
+// })
 
 router.get('/especializaciones', function (req, res) {
     res.render('../Frontend/views/pages/tablas-admin/especializaciones')
